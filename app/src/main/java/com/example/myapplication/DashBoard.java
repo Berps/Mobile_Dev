b8cbc7560c53728862737a7c88691184e5dca6c7
Berps <167870558+Berps@users.noreply.github.com>
2024-05-05 20:59:50 +0300

    QR Only

--- app/src/main/java/com/example/myapplication/DashBoard.java
+++ app/src/main/java/com/example/myapplication/DashBoard.java
@@ -1,13 +1,46 @@
 package com.example.myapplication;
 
+import android.content.Intent;
 import android.os.Bundle;
-
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 import androidx.appcompat.app.AppCompatActivity;
+import com.google.zxing.integration.android.IntentIntegrator;
+import com.google.zxing.integration.android.IntentResult;
 
 public class DashBoard extends AppCompatActivity {
+    Button scanButton;
+    TextView qrCodeInfo;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.dash_board);
+
+        scanButton = findViewById(R.id.scanButton);
+        qrCodeInfo = findViewById(R.id.qrCodeInfo);
+        scanButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                IntentIntegrator integrator = new IntentIntegrator(DashBoard.this);
+                integrator.setDesiredBarcodeFormats(IntentIntegrator.QR_CODE);
+                integrator.setPrompt("Scan a QR Code");
+                integrator.initiateScan();
+            }
+        });
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        IntentResult result = IntentIntegrator.parseActivityResult(requestCode, resultCode, data);
+        if(result != null) {
+            if(result.getContents() != null) {
+                String qrCodeValue = result.getContents();
+                qrCodeInfo.setText("QR Code: " + qrCodeValue);
+            }
+        } else {
+            super.onActivityResult(requestCode, resultCode, data);
+        }
     }
 }
